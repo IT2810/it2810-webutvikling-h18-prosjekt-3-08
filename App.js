56739ee6ba34a91501d7670b6a5e940b4657ed1d
Frikk Hald Andersen <frikkhaldandersen@dhcp-10-22-3-24.wlan.ntnu.no>
2018-09-28 12:44:52 +0200

    #2 Fått opp App.js i expo

--- App.js
+++ App.js
@@ -2,20 +2,24 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container} >
+                <Text style={styles.text}>McApp</Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#00001b',
+        alignItems: 'center',
+        justifyContent: 'center',
+        },
+    text: {
+        color: "white",
+        fontSize: 20,
+        },
+    });
