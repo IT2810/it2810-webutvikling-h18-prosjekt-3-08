49466953b8f5d7ce8b00eeb72bc112141d8a9c62
axeloh <axel.harstad@gmail.com>
2018-10-05 14:48:10 +0200

    #6 Lagt til TextInput-felt nederst på siden

--- app/components/Todos.js
+++ app/components/Todos.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity } from 'react-native';
 
 
 
 export default class Todos extends React.Component {
+
+
+   constructor(){
+      super()
+      this.state = ({
+         openTodos: [],
+         closedTodos: []
+      })
+   }
+
    render() {
       return (
+
          <View style={styles.container}>
-         <Text>This is the Todos Screen </Text>
+            <Text>This is the Todos Screen </Text>
+            <ScrollView style={styles.scrollContainer}>
+
+            </ScrollView>
+
+            <View style={styles.footer}>
+               <TextInput
+                  style={styles.TextInput}
+                  placeholder='Todo name'
+                  placeholderTextColor='white'
+               >
+
+               </TextInput>
+            </View>
          </View>
       );
    }
@@ -20,5 +44,25 @@ const styles = StyleSheet.create({
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center'
+   },
+   scrollContainer: {
+      flex: 1,
+      marginBottom: 100
+   },
+   footer: {
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      right: 0,
+      zIndex: 10
+   },
+   TextInput: {
+      alignSelf: 'stretch',
+      color: '#fff',
+      padding: 20,
+      backgroundColor: '#252525',
+      borderTopWidth: 2,
+      borderTopColor: '#ededed'
    }
+
 })
