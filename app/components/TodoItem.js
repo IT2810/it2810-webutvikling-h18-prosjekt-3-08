21b2c6d5491338bef3c288ade58b054295c18465
axeloh <axel.harstad@gmail.com>
2018-10-05 22:48:19 +0200

    #6 Lagt til en del logikk for framvisning av todos

--- app/components/TodoItem.js
+++ app/components/TodoItem.js
@@ -4,18 +4,22 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 
 export default class TodoItem extends React.Component {
-   render() {
-      return (
-         <View>
 
+   render() {
+      //console.log(this.props.item);
+      return (
+         <View style={styles.container}>
+            <Text>Hi There!</Text>
          </View>
       );
    }
 }
 
 
-
 const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+   },
    todo: {
       position: 'relative',
       padding: 20,
--- app/components/Todos.js
+++ app/components/Todos.js
@@ -1,69 +1,153 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TextInput, FlatList,
+    TouchableOpacity, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView } from 'react-native';
 
-import TodoItem from './TodoItem';
+//import TodoItem from './TodoItem';
 
 
 export default class Todos extends React.Component {
 
 
-   constructor(props){
-      super(props)
-      this.state = ({
-         todoArray: [],
+   constructor(){
+      super()
+      this.state = {
+         todos: [
+            {
+               task: "Matte4",
+               status: "Pending"
+            },
+            {
+               task: "Sove",
+               status: "Done"
+            },
+            {
+               task: "Pule",
+               status: "Pending"
+            },
+            {
+               task: "Se tv",
+               status: "Pending"
+            },
+            {
+               task: "Lese litt",
+               status: "Done"
+            },
+         ],
 
+         textValue: ""
+      }
+   }
+
+
+   onChangeText(value) {
+      this.setState({
+         textValue: value
+      })
+   }
+
+   addTodo() {
+      this.state.todos.push({
+         task: this.state.textValue,
+         status: "Pending"
       })
    }
 
 
 
    render() {
-
+      //console.log(this.state.todos);
       return (
+         <DismissKeyboard>
+               <View style={styles.container}>
+                  <View style={{flex: 1, marginTop: 22}}>
+                     <FlatList
+                        data={this.state.todos}
+                        keyExtractor={item => item.task}
+                        renderItem={({item, index}) => {
+                           console.log(JSON.stringify(item));
+                           console.log(index);
+                           let statusStyle = item.status == 'Done' ? styles.done : styles.pending
+                           return (
+                              <View style={styles.todoItem}>
+                                 <Text style={styles.textStyle}>{item.task}</Text>
+                                 <Text style={[styles.statusText, statusStyle]}>
+                                    {item.status == 'Done' ? 'Done' : 'Pending'}
+                                 </Text>
+                              </View>
+                              )
+                        }}
+                        >
+                     </FlatList>
+                  </View>
 
-         <View style={styles.container}>
+                  <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+                     <View style={styles.footer}>
+                        <TouchableOpacity>
 
-            <ScrollView style={styles.scrollContainer}>
+                              <TextInput
+                                 style={styles.textInput}
+                                 value={this.state.textValue}
+                                 placeholder="Add a todo"
+                                 onChangeText={(value) => this.onChangeText(value)}
+                              >
+                              </TextInput>
 
-            </ScrollView>
+                        </TouchableOpacity>
+                     </View>
+                  </KeyboardAvoidingView>
 
-            <View style={styles.footer}>
-               <TextInput>
-
-               </TextInput>
-            </View>
-
-            <TouchableOpacity
-               style={styles.addButton}
-               >
-               <Text style={styles.addButtonText}>+</Text>
-            </TouchableOpacity>
-
-         </View>
+               </View>
+         </DismissKeyboard>
       );
    }
 }
+
+const DismissKeyboard = ({children}) => (
+   <TouchableWithoutFeedback
+      onPress= {() => Keyboard.dismiss()}>
+      {children}
+   </TouchableWithoutFeedback>
+)
 
 
 const styles = StyleSheet.create({
    container: {
       flex: 1,
       backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center'
+
    },
-   scrollContainer: {
+   todoItem: {
       flex: 1,
-      marginBottom: 100
+      alignItems: 'center',
+      borderColor: '#ddd',
+      borderBottomWidth: 1,
+      padding: 20,
+      backgroundColor: "#fff",
+      justifyContent: 'flex-start',
+      flexDirection: 'column'
+   },
+   textStyle: {
+      alignSelf: 'flex-start',
+      fontSize: 16
+   },
+   statusText: {
+      alignSelf: 'flex-end',
+      fontSize: 15
+   },
+   done: {
+      color: 'green'
+   },
+   pending: {
+      color: 'red'
    },
    footer: {
       position: 'absolute',
       bottom: 0,
       left: 0,
       right: 0,
-      zIndex: 10
+      zIndex: 10,
    },
-   TextInput: {
+   textInput: {
       alignSelf: 'stretch',
       color: '#f2f2f2',
       padding: 20,
@@ -71,22 +155,5 @@ const styles = StyleSheet.create({
       borderTopWidth: 2,
       borderTopColor: '#ededed'
    },
-   addButton: {
-      position: 'absolute',
-      zIndex: 11,
-      right: 20,
-      bottom: 90,
-      backgroundColor: '#e91e63',
-      width: 90,
-      height: 90,
-      borderRadius: 50,
-      alignItems: 'center',
-      justifyContent: 'center',
-      elevation: 8
-   },
-   addButtonText: {
-      color: '#fff',
-      fontSize: 24
-   }
 
 })
