29afff1fc6c04fc38c0374da1b3fe4efce5fdb9a
Eirik Dahlen <dahleneirik@gmail.com>
2018-10-18 22:51:06 +0200

    #12 Lagt til test for Async

--- __tests__/Contacts-test.js
+++ __tests__/Contacts-test.js
@@ -4,7 +4,15 @@ import renderer from 'react-test-renderer';
 
 import Contacts from './app/components/Contacts';
 
-it("Testing", () => {
+
+it("Renders correctly", () => {
     const tree = renderer.create(<Contacts/>).toJSON();
     expect(tree).toMatchSnapshot();
 });
+
+it("Testing Async - store contacts", async () => {
+    expect.assertions(1);
+    const data = await retrieveContacts();
+    expect(data).toEqual(this.state.contacts);
+
+})
