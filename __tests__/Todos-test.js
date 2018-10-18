8162115f584ebd7525e83fb7354ffdf216250bda
Eirik Dahlen <dahleneirik@gmail.com>
2018-10-18 23:12:45 +0200

    #12 Lagt til test for Todos

--- __tests__/Todos-test.js
+++ __tests__/Todos-test.js
@@ -2,16 +2,10 @@ import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import Contacts from './app/components/Contacts';
+import Todos from './app/components/Contacts';
 
 
 it("Renders correctly", () => {
     const tree = renderer.create(<Contacts/>).toJSON();
     expect(tree).toMatchSnapshot();
-});
-
-it("Testing Async - store contacts", async () => {
-    expect.assertions(1);
-    const data = await retrieveContacts();
-    expect(data).toEqual(this.state.contacts);
 });
